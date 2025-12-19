bb008f37c5c305a2acbefe7c6db6a1522b73f722
nahak-rajeswari-17 <nahakrajeswari01@gmail.com>
2025-12-19 21:57:10 +0530

    added yml and updated discovery server application file

--- discovery-server/src/main/java/com/example/discovery/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/discovery/discovery_server/DiscoveryServerApplication.java
@@ -2,12 +2,14 @@ package com.example.discovery.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  // <-- This is mandatory
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
+
